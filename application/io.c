4be8c2740a2cd33dddbbfa38f49bb9658ad5824e
T0n0T <823478402@qq.com>
2023-09-27 17:04:48 +0800

    init

---
+++ application/io.c
@@ -0,0 +1,21 @@
+/**
+ * @file io.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-27
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <stdio.h>
+#include "gd32f450i_eval.h"
+
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(EVAL_COM0, (uint8_t)ch);
+    while (RESET == usart_flag_get(EVAL_COM0, USART_FLAG_TBE))
+        ;
+    return ch;
+}

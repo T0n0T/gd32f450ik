9863ed4d1bdbae304be5cdd17ec35706a59f84ec
T0n0T <823478402@qq.com>
2025-06-17 03:51:59 +0000

    bootloader init

--- application/main.c
+++ application/main.c
@@ -1,112 +1,77 @@
-/*!
-    \file    main.c
-    \brief   USART printf demo
-
-    \version 2016-08-15, V1.0.0, demo for GD32F4xx
-    \version 2018-12-12, V2.0.0, demo for GD32F4xx
-*/
-
-/*
-    Copyright (c) 2018, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice,
-       this list of conditions and the following disclaimer in the documentation
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors
-       may be used to endorse or promote products derived from this software without
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-OF SUCH DAMAGE.
-*/
-
 #include "gd32f4xx.h"
 #include "gd32f450i_eval.h"
 #include <stdio.h>
 #include "systick.h"
 
-void led_init(void);
-void led_flash(int times);
+// 定义跳转地址宏，编译时传入
+#ifndef JUMP_ADDRESS
+#define JUMP_ADDRESS 0x08008000 // 默认跳转地址，可根据实际情况修改
+#endif
 
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
+void main(void)
 {
-    /* initialize the LEDs */
-    led_init();
+    typedef void (*app_func_t)(void);
+    uint32_t app_addr   = JUMP_ADDRESS;
+    uint32_t stk_addr   = *((__IO uint32_t *)app_addr);
+    app_func_t app_func = (app_func_t)(*((__IO uint32_t *)(app_addr + 4)));
 
-    /* configure systick */
-    systick_config();
+    if ((((uint32_t)app_func & 0xff000000) != 0x08000000) || (((stk_addr & 0x2ff00000) != 0x20000000) && ((stk_addr & 0x2ff00000) != 0x24000000))) {
+        printf("No legitimate application.\r\n");
+        return;
+    }
 
-    /* flash the LEDs for 2 times */
-    led_flash(2);
+    printf("Jump to application running ... \r\n");
+    delay_1ms(20);
 
-    /* configure EVAL_COM0 */
-    gd_eval_com_init(EVAL_COM0);
+    // __disable_irq();
 
-    /* configure TAMPER key */
-    gd_eval_key_init(KEY_TAMPER, KEY_MODE_GPIO);
-    printf("\r\nHELLO!!! OpenHARMONY\r\n");
-    extern void RunTaskSample(void);
-    RunTaskSample();
+    SysTick->CTRL = 0;
+    SysTick->LOAD = 0;
+    SysTick->VAL  = 0;
 
-    /* never return here*/
-    return 0;
-}
+    for (int i = 0; i < 128; i++) {
+        NVIC_DisableIRQ((IRQn_Type)i);
+        NVIC_ClearPendingIRQ((IRQn_Type)i);
+    }
 
-/*!
-    \brief      initialize the LEDs
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void led_init(void)
-{
-    gd_eval_led_init(LED1);
-    gd_eval_led_init(LED2);
-    gd_eval_led_init(LED3);
-}
+    __set_CONTROL(0);
+    __set_MSP(stk_addr);
 
-/*!
-    \brief      flash the LEDs for test
-    \param[in]  times: times to flash the LEDs
-    \param[out] none
-    \retval     none
-*/
-void led_flash(int times)
-{
-    int i;
-    for (i = 0; i < times; i++) {
-        /* delay 400 ms */
-        delay_1ms(400);
+    app_func(); // Jump to application running
 
-        /* turn on LEDs */
-        gd_eval_led_on(LED1);
-        gd_eval_led_on(LED2);
-        gd_eval_led_on(LED3);
-
-        /* delay 400 ms */
-        delay_1ms(400);
-        /* turn off LEDs */
-        gd_eval_led_off(LED1);
-        gd_eval_led_off(LED2);
-        gd_eval_led_off(LED3);
+    printf("Qboot jump to application fail.\r\n");
+    while (1) {
+        // 如果跳转失败，可以在这里添加错误处理或指示
     }
 }
+
+#if defined(__GNUC__)
+ssize_t _write(int fd, char *ptr, int len)
+{
+    int i = 0;
+
+    /*
+     * write "len" of char from "ptr" to file id "fd"
+     * Return number of char written.
+     *
+     * Only work for STDOUT, STDIN, and STDERR
+     */
+    if (fd > 2) {
+        return -1;
+    }
+
+    while (*ptr && (i < len)) {
+        usart_data_transmit(EVAL_COM0, *(uint8_t *)ptr);
+        while (RESET == usart_flag_get(EVAL_COM0, USART_FLAG_TBE));
+        if (*ptr == '\n') {
+            usart_data_transmit(EVAL_COM0, '\r');
+            while (RESET == usart_flag_get(EVAL_COM0, USART_FLAG_TBE));
+        }
+
+        i++;
+        ptr++;
+    }
+
+    return i;
+}
+#endif

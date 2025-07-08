36ed5185bd4d7f975fb83a725a60a6396b3eb580
T0n0T <823478402@qq.com>
2025-07-08 16:14:28 +0000

    define SYSCALL for limtied 4 param

--- application/main.c
+++ application/main.c
@@ -7,15 +7,12 @@
 uint32_t hellobss[4]  = {0};
 uint32_t hellodata[4] = {0xa, 0xb, 0xc, 0xd};
 
-SVCALL(1, int, hello_svc(uint32_t arg0, uint32_t arg1, uint32_t arg3));
-SVCALL(2, int, average_svc(uint32_t count, ...));
+SVCALL(1, int, hello_svc(uint32_t arg0, uint32_t arg1, uint32_t arg2));
 
 int main(void)
 {
     memcpy(hellobss, hellodata, sizeof(hellobss));
     printf("svc_1 return: %lu\r\n", hello_svc(1, 2, 3));
-    printf("svc_2 return: %x\r\n", average_svc(3, 1, 2, 3));
-
     while (1) {
     }
 }
--- platform/svc/svc.h
+++ platform/svc/svc.h
@@ -5,6 +5,7 @@
 extern "C" {
 #endif
 
+// in cortex-m,only r0-r3 will be automatically push, so svc param limit to 4
 #define SVCALL(number, return_type, signature)           \
     __attribute__((naked))                               \
     __attribute__((unused)) static return_type signature \

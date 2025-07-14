15d29fa11d436a3358087c34228326e7bb29251d
T0n0T <823478402@qq.com>
2025-07-14 16:24:14 +0000

    update call addr

--- application/main.c
+++ application/main.c
@@ -14,7 +14,7 @@ int main(void)
     memcpy(hellobss, hellodata, sizeof(hellobss));
     printf("svc_1 return: %lu\r\n", hello_svc(1, 2, 3));
     int (*entry)(void);
-    entry = (int (*)(void))0x08010000;
+    entry = (int (*)(void))0x08010005;
     entry();
     // NEVER REACH HERE
     while (1) {
